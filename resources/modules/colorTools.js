d4294ec3bd274e4249fa5f31896960cd0d9b14e4
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-07-30 16:13:53 +0100

    Create colorTools.js

---
+++ resources/modules/colorTools.js
@@ -0,0 +1,12 @@
+exports.generateRandomHexColor = function () {
+    let hexValues = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hexValues[Math.floor(Math.random() * hexValues.length)]
+    }
+    return color;
+}
+
+exports.generateRandomHSLColor = function () {
+    return `hsl(${Math.floor(Math.random() * 360)}, 70%, 26%)`;
+}
