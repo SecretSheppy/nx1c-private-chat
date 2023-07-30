b6b45d77c64e9ddb93d353db5b9dda45783c9689
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-07-30 16:13:49 +0100

    mTree compatibility

--- resources/modules/fileTools.js
+++ resources/modules/fileTools.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
+const mTree = require('./mTree');
 
 exports.loadJson = function (jsonPath) {
     return JSON.parse(fs.readFileSync(jsonPath));
+}
+
+exports.saveJson = function (jsonPath, jsonData) {
+    fs.writeFileSync(jsonPath, JSON.stringify(jsonData, null, 4));
+}
+
+exports.loadMessageTree = function (messageTreePath) {
+    return new mTree.MessageTree(fs.readFileSync(messageTreePath).toString());
+}
+
+exports.appendToRawMessageTree = function (messageTreePath, data) {
+    fs.appendFileSync(messageTreePath, JSON.stringify(data) + "\n");
 }
