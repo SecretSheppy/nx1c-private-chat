015a88f0a4a0cd26c03af01c5bce0deca00f8706
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-07-30 16:14:21 +0100

    Refactor and mTree Compatibility

--- resources/js/client.js
+++ resources/js/client.js
@@ -2,18 +2,24 @@ class ClientEnvironment {
 
     socket;
     privateKey;
+    clientColor = "";
+    clientConfigPath = './configuration/client-config.json';
+    previousConnectionsPath = './configuration/previous-connections.json';
 
-    constructor (configPath) {
+    constructor () {
         this.privateKey = generateRandomPrivateKey(16);
+        this.clientConfig = fileTools.loadJson(this.clientConfigPath);
+        this.previousConnections = fileTools.loadJson(this.previousConnectionsPath);
     }
 
     connectToServer () {
-        let serverHostElement = document.getElementById("server-host");
-        let usernameElement = document.getElementById("username");
-        if (serverHostElement.value !== null && usernameElement.value !== null) {
-            this.socket = io.connect(document.getElementById("protocol").value + serverHostElement.value);
+        this.serverProtocol = document.getElementById("protocol").value;
+        this.serverHost = document.getElementById("server-host").value;
+        this.username = document.getElementById("username").value;
+        if (this.serverHost !== null && this.username !== null) {
+            this.socket = io.connect(this.serverProtocol + this.serverHost);
+            $("#alert").fadeIn(500);
             this.appendSocketScript();
-            this.username = usernameElement.value;
             // TODO - build a better loading connection indicator
             // document.getElementById("alert").style.display = "block";
         }
@@ -29,17 +35,113 @@ class ClientEnvironment {
     submitServerPassword () {
         this.socket.emit("advance-through-queue", {
             password: document.getElementById("password-text").value,
-            username: encrypt(this.privateKey, this.username)
+            username: encrypt(this.privateKey, this.username),
+            color: this.clientColor
         })
     }
 
     sendMessage () {
         let messageComposerElement = document.getElementById("message-box")
-        this.socket.emit("new message", {
-            "username": encrypt(this.privateKey, this.username),
-            "message": encrypt(this.privateKey, messageComposerElement.value)
+        if (messageComposerElement.value !== "") {
+            this.socket.emit("new message", {
+                username: encrypt(this.privateKey, this.username),
+                message: encrypt(this.privateKey, messageComposerElement.value),
+                color: encrypt(this.privateKey, this.clientColor)
+            });
+            messageComposerElement.value = "";
+        };
+    }
+
+    transitionBetweenPages (currentPages, targetPages) {
+        currentPages.forEach(page => {
+            $(page).fadeOut(500);
         });
-        messageComposerElement.value = "";
+        setTimeout(() => {
+            targetPages.forEach(page => {
+                $(page).fadeIn(500);
+            });
+        }, 500);
+    }
+
+    showHome () {
+        this.transitionBetweenPages(["#kicked"], ["#login", "#recent-connections"]);
+    }
+
+    loadRecentConnections () {
+        for (let address in this.previousConnections) {
+            let connectionElement = document.createElement("div");
+            connectionElement.classList.add("connection");
+            connectionElement.setAttribute("onclick", "loadPreviousConnection(this);");
+            let addressElement = document.createElement("p");
+            addressElement.innerText = "Address: " + this.previousConnections[address].protocol + address;
+            connectionElement.appendChild(addressElement);
+            let usernameElement = document.createElement("p");
+            usernameElement.innerText = "Username: " + this.previousConnections[address]["username"];
+            connectionElement.appendChild(usernameElement);
+            let colorElement = document.createElement("p");
+            colorElement.innerText = "Color: " + this.previousConnections[address]["color"];
+            connectionElement.appendChild(colorElement);
+            document.getElementById("connections").appendChild(connectionElement);
+        }
+    }
+
+    updatePrivateKey () {
+        let privateKeyElement = document.getElementById("private-key-field");
+        this.previousConnections[this.serverHost].privateKey = this.privateKey = privateKeyElement.value;
+        fileTools.saveJson(this.previousConnectionsPath, this.previousConnections);
+        $("#key-modifier").fadeOut(200);
+        setTimeout(() => {
+            privateKeyElement.value = "";
+        }, 200);
+    }
+
+    createMessageElement (messageData, position = "post") {
+        let decryptedUsername = decrypt(client.privateKey, messageData.username);
+        let decryptedMessage = decrypt(client.privateKey, messageData.message);
+        let decryptedColor = decrypt(client.privateKey, messageData.color);
+        let newMessageElement = document.createElement("div");
+        newMessageElement.classList.add("message");
+        let userIconElement = document.createElement("div");
+        userIconElement.classList.add("user-icon");
+        userIconElement.style.backgroundColor = decryptedColor;
+        userIconElement.innerText = decryptedUsername[0].toUpperCase();
+        newMessageElement.appendChild(userIconElement);
+        let messageWrapperElement = document.createElement("div");
+        messageWrapperElement.classList.add("message-wrapper");
+        messageWrapperElement.style.backgroundColor = decryptedColor;
+        let usernameElement = document.createElement("h4");
+        usernameElement.innerText = decryptedUsername;
+        messageWrapperElement.appendChild(usernameElement);
+        let messageTextElement = document.createElement("p");
+        messageTextElement.innerText = decryptedMessage;
+        messageWrapperElement.appendChild(messageTextElement);
+        newMessageElement.appendChild(messageWrapperElement);
+        if (position !== "pre") {
+            document.getElementById("message-list").appendChild(newMessageElement);
+        } else {
+            document.getElementById("message-list").prepend(newMessageElement);
+        }
+        this.scrollToBottom();
+    }
+
+    createUserActiveElement (userData, position = "post") {
+        let decryptedUsername = decrypt(client.privateKey, userData.username);
+        let userJoinedWrapper = document.createElement("div");
+        userJoinedWrapper.classList.add("user-joined-wrapper");
+        let usernameWrapper = document.createElement("div");
+        usernameWrapper.classList.add("username-wrapper");
+        usernameWrapper.innerText = decryptedUsername + " joined the server";
+        userJoinedWrapper.appendChild(usernameWrapper);
+        if (position !== "pre") {
+            document.getElementById("message-list").appendChild(userJoinedWrapper);
+        } else {
+            document.getElementById("message-list").prepend(userJoinedWrapper);
+        }
+        this.scrollToBottom();
+    }
+
+    scrollToBottom () {
+        document.getElementById("message-list").scrollTop = document.getElementById("message-list").scrollHeight;
     }
 
 }
--- resources/js/clientSocket.js
+++ resources/js/clientSocket.js
@@ -5,52 +5,49 @@ client.socket.on("joined-server-queue", (passwordData) => {
             password: null
         });
     } else {
-        document.getElementById("recent-connections").style.display = "none";
-        document.getElementById("login").style.display = "none";
-        document.getElementById("password").style.display = "flex";
+        client.transitionBetweenPages(["#recent-connections", "#login", "#alert"], ["#password"]);
     }
 });
 
-client.socket.on("advance-client-ui", () => {
-    document.getElementById("password").style.display = "none";
-    document.getElementById("chat").style.display = "block";
+client.socket.on("advance-client-ui", (data) => {
+    if (data.color) {
+        client.clientColor = data.color;
+    }
+    if (!Object.keys(client.previousConnections).includes(client.serverHost)) {
+        client.previousConnections[client.serverHost] = {
+            "protocol": client.serverProtocol,
+            "username": client.username,
+            "color": client.clientColor,
+            "privateKey": client.privateKey
+        }
+        fileTools.saveJson(client.previousConnectionsPath, client.previousConnections);
+    }
+    client.transitionBetweenPages(["#password"], ["#chat", "#key-button"]);
+    client.socket.emit("load-messages-from-backlog", { quantity: client.clientConfig.messagesToLoadFromBacklog });
 });
 
 client.socket.on("message", (messageData) => {
-    let decryptedUsername = decrypt(client.privateKey, messageData["username"]);
-    let decryptedMessage = decrypt(client.privateKey, messageData["message"]);
-    let newMessageElement = document.createElement("div");
-    newMessageElement.classList.add("message");
-    let userIconElement = document.createElement("div");
-    userIconElement.classList.add("user-icon");
-    userIconElement.innerText = decryptedUsername.at(0);
-    newMessageElement.appendChild(userIconElement);
-    let messageWrapperElement = document.createElement("div");
-    messageWrapperElement.classList.add("message-wrapper");
-    let usernameElement = document.createElement("h4");
-    usernameElement.innerText = decryptedUsername;
-    messageWrapperElement.appendChild(usernameElement);
-    let messageTextElement = document.createElement("p");
-    messageTextElement.innerText = decryptedMessage;
-    messageWrapperElement.appendChild(messageTextElement);
-    newMessageElement.appendChild(messageWrapperElement);
-    document.getElementById("message-list").appendChild(newMessageElement);
+    client.createMessageElement(messageData);
 });
 
 client.socket.on("user-advanced-to-active", (userData) => {
-    let decryptedUsername = decrypt(client.privateKey, userData.username);
-    let userJoinedWrapper = document.createElement("div");
-    userJoinedWrapper.classList.add("user-joined-wrapper");
-    let usernameWrapper = document.createElement("div");
-    usernameWrapper.classList.add("username-wrapper");
-    usernameWrapper.innerText = decryptedUsername + " joined the server";
-    userJoinedWrapper.appendChild(usernameWrapper);
-    document.getElementById("message-list").appendChild(userJoinedWrapper);
-})
+    client.createUserActiveElement(userData);
+});
 
 client.socket.on("user-kicked-from-queue", (reason) => {
     client.socket.disconnect();
-    document.getElementById("alert").innerText = "You have been kicked from the server!";
-    document.getElementById("alert").style.display = "block";
-    // TODO - finish the kick procedure.
-})
+    client.transitionBetweenPages(["#password"], ["#kicked"]);
+    document.getElementById("kick-reason").innerText = "Reason: " + reason.reason;
+});
+
+client.socket.on("backlogged-message", (messageData) => {
+    console.log(messageData);
+    switch (messageData.subjectToBroadcast) {
+        case "message":
+            client.createMessageElement(messageData.dataToBroadcast, "pre");
+            break;
+        case "user-advanced-to-active":
+            client.createUserActiveElement(messageData.dataToBroadcast, "pre");
+            break;
+    }
+});
