cf28062735845e2e99b7101b53d7908fe133b7b9
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-02-14 23:29:38 +0000

    Removed quotation marks and added main menu function

--- run.bat
+++ run.bat
@@ -1,11 +1,29 @@
 @ECHO off
 
-ECHO "NX1C PRIVATE CHAT: MAIN MENU"
-ECHO "1. Start Server"
-ECHO "2. Start Client"
-ECHO "3. Start Server and Client"
-ECHO "4. Check for updates"
-ECHO "5. Exit"
+CALL :main_menu
+
+PAUSE
+EXIT /B 0
+
+:start_server
+ECHO Starting Server...
+node server2.js
+EXIT /B 0
+
+:start_client
+ECHO Starting Client..
+nw.exe
+EXIT /B 0
+
+:main_menu
+ECHO ---------------------------------------------
+ECHO NX1C PRIVATE CHAT: MAIN MENU
+ECHO ---------------------------------------------
+ECHO 1. Start Server
+ECHO 2. Start Client
+ECHO 3. Start Server and Client
+ECHO 4. Check for updates
+ECHO 5. Exit
 
 SET /p choice="Option: "
 
@@ -20,22 +38,14 @@ IF %choice%==3 (
     CALL :start_server
     )
 IF %choice%==4 (
-    ECHO "Checking for updates..."
+    ECHO Checking for updates...
     git pull
+    ECHO Returning to main menu in 5 seconds...
+    TIMEOUT /T 5
+    cls
+    CALL :main_menu
     )
 IF %choice%==5 (
-    ECHO "Exiting..."
+    ECHO Exiting...
     )
-
-PAUSE
-EXIT /B 0
-
-:start_server
-ECHO "Starting Server..."
-node server2.js
-EXIT /B 0
-
-:start_client
-ECHO "Starting Client..."
-nw.exe
 EXIT /B 0
